49c1a4e51682b772c40f11a7a611be9eca0ff76f
Jessica <jessicakorubo@gmail.com>
2023-02-17 06:19:55 +0100

    Fixed mobile navbar

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,17 @@
 
 const hamburger = document.querySelector('.hamburger');
 // const hero = document.querySelector('.hero');
-// const close = document.querySelector('.closeButton');
+const navbar = document.querySelector('.navbar');
+const close = document.querySelector('.close');
 
 hamburger.addEventListener('click', ()=>{
     navbar.classList.add('active');
-    hero.style.zIndex="-1";
+    // hero.style.zIndex="-1";
 });
 
-// close.addEventListener ('click', ()=>{
-//     navbar.classList.remove('active');
-// })
+close.addEventListener ('click', ()=>{
+    navbar.classList.remove('active');
+})
 
 
 //changing the nav background color on scroll 
@@ -19,6 +20,6 @@ const mobilenavbar = document.querySelector(".mobile-navbar");
 document.addEventListener('scroll', ()=>{
     var scrollPosition = window.scrollY;
     if (scrollPosition > 150){
-        document.style.backgrounColor="pink";
+        mobilenavbar.style.backgroundColor="pink";
     }
 });
