6777ee4c90dba83c13f3308667a0c47e4e0dd2f0
Jessica <jessicakorubo@gmail.com>
2023-02-23 14:03:01 +0100

    new commit

--- javascript/app.js
+++ javascript/app.js
@@ -3,7 +3,8 @@ const hamburger = document.querySelector('.hamburger');
 // const hero = document.querySelector('.hero');
 const navbar = document.querySelector('.navbar');
 const close = document.querySelector('.close');
-const menuLink = document.querySelectorAll(".menu a")
+const menuLink = document.querySelectorAll(".menu a");
+const video = document.querySelector(".background-video");
 
 hamburger.addEventListener('click', ()=>{
     navbar.classList.add('active');
@@ -13,6 +14,16 @@ close.addEventListener ('click', ()=>{
     navbar.classList.remove('active');
     
 })
+
+
+// Function for after play video dissapearance.
+videoEnd = ()=>{
+    video.addEventListener('ended', ()=>{
+        console.log("Finished video");
+        video.style.display= "none";
+    });
+}
+// videoEnd();
 
 //MAKING THE NAVBAR DISSAPEAR WHEN I CLICK ON IT
 disactivateNav = ()=> {
@@ -25,13 +36,13 @@ setTimeout(disactivateNav,2000);
 //changing the nav background color on scroll 
 const mobilenavbar = document.querySelector(".mobile-navbar");
 
-document.addEventListener('scroll', ()=>{
-    var scrollPosition = window.scrollY;
-    if (scrollPosition > 50){
-        mobilenavbar.style.backgroundColor="black";
-    }
-    else if (scrollPosition <50){
-        mobilenavbar.style.backgroundColor="transparent";
-    }
-});
+// document.addEventListener('scroll', ()=>{
+//     var scrollPosition = window.scrollY;
+//     if (scrollPosition > 50){
+//         mobilenavbar.style.backgroundColor="black";
+//     }
+//     else if (scrollPosition <50){
+//         mobilenavbar.style.backgroundColor="transparent";
+//     }
+// });
 
